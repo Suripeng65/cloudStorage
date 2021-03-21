9538aaeac1feeb786a18ab94d64e8c696e14e8ee
Suri Peng <peng.siyu@hotmail.com>
2021-03-20 20:50:41 -0700

    add fileMapper and file service to home-file tab

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+
+public interface FileMapper {
+	@Select("SELECT * FROM FILES WHERE filename = #{fileName}")
+	File getFile(String fileName);
+	
+	@Select("SELECT * FROM FILES WHERE fileId = #{fileId}")
+	File getFile(int fileId);
+	
+    @Select("SELECT filename FROM FILES WHERE userid = #{userId}")
+    String[] getFileListings(Integer userId);
+	
+	@Insert("INSERT INTO FILES ((filename, contenttype, filesize, userid, filedata) VALUES (#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
+	@Options(useGeneratedKeys = true, keyProperty="fileId")
+	int insert(File file);
+	
+	@Delete("DELETE FROM FILES WHERE filename = #{fileName}")
+	void deleteFile(String fileName);
+}
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,51 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+
+public class FileService {
+	private FileMapper fileMapper;
+	private UserMapper userMapper;
+	
+	public FileService(FileMapper fileMapper, UserMapper userMapper) {
+		super();
+		this.fileMapper = fileMapper;
+		this.userMapper = userMapper;
+	}
+
+	public void deleteFile(File file) {
+		fileMapper.deleteFile(file.getFileName());
+	}
+	
+	public int addFile(MultipartFile multipartFile, String username) throws IOException {
+		InputStream fis = multipartFile.getInputStream();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = fis.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        byte[] fileData = buffer.toByteArray();
+
+        String fileName = multipartFile.getOriginalFilename();
+        String contentType = multipartFile.getContentType();
+        String fileSize = String.valueOf(multipartFile.getSize());
+        Integer userId = userMapper.getUserByName(username).getuserId();
+		return fileMapper.insert(new File(0, fileName, contentType, fileSize, userId, fileData));
+	}
+	
+	public File getFile(String fileName) {
+		return fileMapper.getFile(fileName);
+	}
+	public String[] getFileListing(int userId) {
+		return fileMapper.getFileListings(userId);
+	}
+}
